14feee04f51d0f80c4fe2d49908c018c42a70aef
AlloJS <eventorganization1@gmail.com>
2024-11-09 11:09:22 +0100

    first commit

---
+++ Question App/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  Question App
+//
+//  Created by Angelo Bellanca on 08/11/24.
+//
+
+import Foundation
---
+++ Question App/listsQuestions.swift
@@ -0,0 +1,8 @@
+//
+//  listsQuestions.swift
+//  Question App
+//
+//  Created by Angelo Bellanca on 09/11/24.
+//
+
+import Foundation
